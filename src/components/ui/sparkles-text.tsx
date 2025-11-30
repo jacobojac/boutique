41002f8e4892c43237163c5d8d3f10e3c3763b31
Walid Benmouffok <walidbenmouffok@MacBook-Pro-de-Walid.local>
2025-11-30 20:50:04 +0100

    rectif couleurs sparkles

--- src/components/ui/sparkles-text.tsx
+++ src/components/ui/sparkles-text.tsx
@@ -86,7 +86,7 @@ interface SparklesTextProps {
 
 export const SparklesText: React.FC<SparklesTextProps> = ({
   children,
-  colors = { first: "#9E7AFF", second: "#FE8BBB" },
+  colors = { first: "#FFC107", second: "#FFD54F" },
   className,
   sparklesCount = 10,
   ...props
