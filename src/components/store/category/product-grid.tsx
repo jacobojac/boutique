671674de06a693d95dff5937664b097d4cc00e5a
Walid Benmouffok <walidbenmouffok@MacBook-Pro-de-Walid.local>
2025-11-30 21:46:11 +0100

    rectif 2 produits par ligne

--- src/components/store/category/product-grid.tsx
+++ src/components/store/category/product-grid.tsx
@@ -24,7 +24,7 @@ export function ProductGrid({ products }: ProductGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {activeProducts.map((product: TypeProduct) => (
         <ProductCard key={product.id} product={product} />
       ))}
